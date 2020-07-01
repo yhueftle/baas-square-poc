0b6ba0a9b7237778a48572a339a0286c0f6f3ea3
Yannick Hueftle <yhueftle@gmail.com>
2020-06-30 20:29:40 -0500

    First commit

---
+++ src/main/java/com/baas/squarepoc/SquarePocApplication.java
@@ -0,0 +1,13 @@
+package com.baas.squarepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquarePocApplication.class, args);
+	}
+
+}
